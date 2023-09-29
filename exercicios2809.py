58e2632d16161c9ec0bb82aa12315955e6aad0e8
runghh <rungecraft3@gmail.com>
2023-09-28 22:03:59 -0300

    exercicios28/09

---
+++ exercicios2809.py
@@ -0,0 +1,66 @@
+#exercicios 1
+# 2 numero / se 1 numero for impar ( vai somar todos os numeros ate o ultimo)
+# se o segundo for par (vai somar todos os numeros)
+
+#numA = int(input("Digite o primeiro numero: "))
+#numB = int(input("Digite o segundo numero: "))
+
+#numB = numB + 1
+#soma = 0
+
+#if numA%2 == 1:
+# print("numeros impares:")
+#else:
+# print("numeros pares:")
+#
+#for i in range(numA,numB,2):
+# 
+# if i%2 == 1:
+#  soma = soma + i
+#   print(soma)
+# else:
+#   soma = soma + i
+#   print(soma)
+
+#exercicio 2
+#
+#while True:
+# nota = int(input("Digite a nota entre 0 e 10: "))
+# if nota > 0 and nota <= 10 :
+#  print("Nota Correta")
+#  break
+# else:
+#  print("Nota Invalida")
+
+#Exercicio 3
+# faça um programa que leia 5 numeros e informe o maior numero
+
+
+#nummaior = int(input("Digite um numero: "))
+#for i in range(1,5):
+# numB = int(input("Digite um numero: "))
+# if nummaior < numB :
+#  nummaior = numB
+#print("Maior Numero: ", nummaior)
+
+#Suponha que a populçao de um pais A seja 8000 taxa anaul 2%
+#pais B 20.000 1.5% ANOS NECESSARIO
+
+
+#anos = 0
+#paisA = int(input("Digite o numero de civilização do pais A: "))
+#PA = float(input("Porcentagem de crescimento por ano:"))
+#paisB = int(input("Digite o numero de civilização do pais B: "))
+#PB = float(input("Porcentagem de crescimento por ano:"))
+
+#while True:
+
+# paisA = paisA+(paisA*(PA/100))
+# paisB = paisB+(paisB*(PB/100))
+# anos = anos + 1
+
+# if paisB <= paisA:
+#     break
+
+
+#print("O pais A ira demora ",anos,"para chegar ao pais B")
